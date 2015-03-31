a76687f9ffe4eb179dca8d58a15112ee83f634fe
Rolf Behrens <mail@rolfbehrens.net>
2015-03-31 12:20:14 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # FileUploadToMongoDBGFSandExpress
 A MongoDB GridFS file server example written with Express and Multer
+
+
+# Dependencies 
+
+This example uploads files and stores them in MongoDB's GridFS. 
+
+For this example to run, you need to install MongoDB on your System.
+
+On OSX (with Homebrew)
+
+    brew install mongodb 
+
+On Ubuntu 
+
+    apt-get install mongodb
+
+For any other systems see:
+
+https://www.mongodb.org/downloads 
+
+## Start MongoDb 
+
+Create a dbpath first:
+
+    mkdir ~/dbpath 
+
+Start MongoDb
+    
+    mongodb --path ~/dbpath
+
+
+
+# Initializiation 
+
+Install all required NodeJS - dependencies 
+
+
+    npm install 
+
+
+# Start Server 
+
+    node server.js 
+
+
+
+# Test Upload
+
+###Example Page to upload Files: 
+
+    http://localhost:<port>/
+
+### Get MetaData of uploaded Files 
+
+    http://localhost:<port>/files/<filename> 
+
+### Download File
+
+    http://localhost:<port>/files/get/<filename>
---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var multer  = require('multer');
+var fs = require('fs');
+var app = express();
+
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var Grid = require('gridfs-stream');
+
+var basicAuth = require('basic-auth-connect');
+
+var gfs;
+var db;
+
+// Authenticator
+app.use(basicAuth('fileUploadUser', 'iC4nh4ndl3F1l3s'));
+
+app.use('/',       express.static(__dirname + '/client', { maxAge: 0 }));
+
+
+app.post('/upload',  multer({
+    dest:"./tmp/",
+  }),
+  function(req,res) {
+
+    var fileMulterName = req.files.file.path;
+    var filename = req.files.file.originalname;
+    var metadata = {
+      userName : req.body.userName
+    };
+
+    this.writestream = gfs.createWriteStream({
+        filename:filename,
+        mode:"w",
+        chunkSize:1024*256,
+        content_type:req.files.file.mimetype,
+        root:"fs",
+        metadata:metadata
+    });
+
+    fs.createReadStream(fileMulterName).pipe(writestream);
+
+    writestream.on('close', function (file) {
+      // do something with `file`
+      res.sendStatus(200);
+
+      fs.unlink(fileMulterName, function (err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log("successfully deleted temporary File from FS.");
+        }
+      });
+    });
+});
+
+app.get('/file/get/:file', function(req,res) {
+  try {
+    var readstream = gfs.createReadStream({filename:req.params.file});
+    readstream.pipe(res);
+  }
+  catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+app.get('/file/:filename', function(req,res) {
+  gfs.files.find({ filename: req.params.filename }).toArray(function (err, files) {
+    if (err) throw err;
+    res.send(files);
+  });
+});
+
+
+
+// Initialize MongoDB connection once
+MongoClient.connect("mongodb://localhost/test", function(err, database) {
+  if(err) throw err;
+  db = database;
+  gfs = Grid(db, mongo);
+});
+
+app.listen(8080); // <<---- Start Listeining to Port
+
+
+
